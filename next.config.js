e518f698060379d363c5e799d02f9dba855c7ff5
Michella <michella.reaidy@gmail.com>
2025-04-21 22:36:25 +0200

    adjusting package lock

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+    ],
+  },
+  reactStrictMode: true, // Safe and useful
+};
+
+module.exports = nextConfig;
